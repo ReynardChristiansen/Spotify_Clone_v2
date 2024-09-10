172837b17d3b31742a16f06c87e078127583a316
unknown <reynard.satria@gmail.com>
2024-09-10 12:00:28 +0700

    undo remove cookies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,8 +20,7 @@ const Navbar = () => {
     Cookies.remove('name');
     Cookies.remove('role');
     Cookies.remove('id');
-    Cookies.remove('volume');
-    setUserName('')
+    setUserName('');
     // Redirect to the login page
     navigate('/')
     window.location.reload();
